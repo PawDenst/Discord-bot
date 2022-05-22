6098e4d6da82ebcd08f7c99fd146aaa57b456f5c
Pawel <01149590@pw.edu.pl>
2022-05-22 21:16:38 +0200

    poprawa plikow

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import json
 import music
 import random
 from random import randint
+from random import choice
 from replit import db
 from keep_alive import keep_alive
 import datetime
@@ -18,17 +19,22 @@ from openpyxl import load_workbook
 
 
 
-#import all of the cogs
-
-
+#import all of the cogs#os.system('pip install pymongo[srv]')
 #client = discord.Client()
 
 cogs =[music]
 
 client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
 
+
 for i in range(len(cogs)):
     cogs[i].setup(client)
+
+
+
+
+
+
 sad_words = [
     "depresja", "smutek", "złość", "angry", "oint", "depressing", "dobroć",
     "odpisz", "napisz", "powiedz"
@@ -41,6 +47,7 @@ matlab = ["matlab"]
 memy = ["meme", "memy", "memowanie"]
 
 zajecia = ["zajęcia", "zajecia", "Zajęcia", "Zajecia"]
+dzis = ['dzis zajecia' , "dziś zajęcia","dzis zajęcia","dziś zajecia","zajecia dzis", "zajęcia dziś", "zajęcia dzis", "zajecia dziś"]
 
 studia = ["terminarz", "kolokwia", "kolosy", "kolosa", "kolokwium"]
 
@@ -105,8 +112,8 @@ async def on_message(message):
 
   msg = message.content
 
-  
 
+  
   if msg.startswith('inspire'):
       quote = get_quote()
       await message.channel.send(quote)
@@ -161,73 +168,47 @@ async def on_message(message):
           db["responding"] = False
           await message.channel.send("Responding is off.")
 
-  if any(word.lower() in msg.lower() for word in memy):
-      num = randint(1, 8)
-      try:
-          await message.channel.send(file=discord.File("{}.jpg".format(num)))
-      except:
-          await message.channel.send(file=discord.File("{}.png".format(num)))
+  # if any(word.lower() in msg.lower() for word in memy):
+  #     num = randint(1, 8)
+  #     try:
+  #         await message.channel.send(file=discord.File("{}.jpg".format(num)))
+  #     except:
+  #         await message.channel.send(file=discord.File("{}.png".format(num)))
 
-  if any(word.lower() in msg.lower() for word in matlab):
-      num = randint(9, 9)
-      try:
-          await message.channel.send(file=discord.File("{}.jpg".format(num)))
-      except:
-          await message.channel.send(file=discord.File("{}.png".format(num)))
+  # if any(word.lower() in msg.lower() for word in matlab):
+  #     num = randint(9, 9)
+  #     try:
+  #         await message.channel.send(file=discord.File("{}.jpg".format(num)))
+  #     except:
+  #         await message.channel.send(file=discord.File("{}.png".format(num)))
 
   if (message.content.startswith("mazury")):
       await message.channel.send(file=discord.File("9.png"))
 
   wb = load_workbook('Zeszyt1.xlsx')
-  sheet = wb.active
+  sheet = wb.active 
   if message.author.id in [406436455888715777, 213390565604196352]:
-    sheet = wb["Arkusz2"]
-  elif message.author.id in [401075326178033685, 631587778815328256, 191655544463818762]:
+    sheet = wb["Solej"]
+  elif message.author.id in [631587778815328256]:
     sheet = wb["Arkusz3"]
+  elif message.author.id in [268863457011564547, 373561751926472704]:
+    sheet = wb["Tramwaj"]
+  elif message.author.id in [74888883061403648]:
+    sheet = wb["Ellis"]
+  elif message.author.id in [401075326178033685]:
+    sheet = wb["Shroomy"]
+  elif message.author.id in [191655544463818762]:
+    sheet = wb["Piwosz"]
+  elif message.author.id in [182554213090328577]:
+    sheet = wb["Axword"]
+  elif message.author.id in [832003804747530250]:
+    sheet = wb["qxv21"]
+  elif message.author.id in [213390565604196352]:
+    sheet = wb["Marcin"]
   else:
     sheet = wb["Arkusz1"]
-  if any(word.lower() in msg.lower() for word in zajecia):
+  if any(word.lower() in msg.lower() for word in dzis):
       if (datetime.today().weekday()) == 0:
-          for row in sheet.iter_rows(min_row=1,
-                                     min_col=3,
-                                     max_row=20,
-                                     max_col=3):
-              for cell in row:
-                  if (str(cell.value)) == "None":
-                      continue
-                  else:
-                      await message.channel.send(cell.value)
-      elif (datetime.today().weekday()) == 1:
-          for row in sheet.iter_rows(min_row=1,
-                                     min_col=4,
-                                     max_row=20,
-                                     max_col=4):
-              for cell in row:
-                  if (str(cell.value)) == "None":
-                      continue
-                  else:
-                      await message.channel.send(cell.value)
-      elif (datetime.today().weekday()) == 2:
-          for row in sheet.iter_rows(min_row=1,
-                                     min_col=5,
-                                     max_row=20,
-                                     max_col=5):
-              for cell in row:
-                  if (str(cell.value)) == "None":
-                      continue
-                  else:
-                      await message.channel.send(cell.value)
-      elif (datetime.today().weekday()) == 3:
-          for row in sheet.iter_rows(min_row=1,
-                                     min_col=6,
-                                     max_row=20,
-                                     max_col=6):
-              for cell in row:
-                  if (str(cell.value)) == "None":
-                      continue
-                  else:
-                      await message.channel.send(cell.value)
-      elif (datetime.today().weekday()) == 6:
           for row in sheet.iter_rows(min_row=1,
                                      min_col=2,
                                      max_row=20,
@@ -237,23 +218,141 @@ async def on_message(message):
                       continue
                   else:
                       await message.channel.send(cell.value)
-      else:
-        await message.channel.send("Jutro nie ma zajęć. gramy gramy GRAMY")      
+      elif (datetime.today().weekday()) == 1:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=3,
+                                     max_row=20,
+                                     max_col=3):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+      elif (datetime.today().weekday()) == 2:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=4,
+                                     max_row=20,
+                                     max_col=4):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+      elif (datetime.today().weekday()) == 3:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=5,
+                                     max_row=20,
+                                     max_col=5):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+      elif (datetime.today().weekday()) == 4:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=6,
+                                     max_row=20,
+                                     max_col=6):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value) 
+  elif any(word.lower() in msg.lower() for word in zajecia):
+    if (datetime.today().weekday()) == 0:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=3,
+                                     max_row=20,
+                                     max_col=3):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+    elif (datetime.today().weekday()) == 1:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=4,
+                                     max_row=20,
+                                     max_col=4):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+    elif (datetime.today().weekday()) == 2:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=5,
+                                     max_row=20,
+                                     max_col=5):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+    elif (datetime.today().weekday()) == 3:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=6,
+                                     max_row=20,
+                                     max_col=6):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+    elif (datetime.today().weekday()) == 6:
+          for row in sheet.iter_rows(min_row=1,
+                                     min_col=2,
+                                     max_row=20,
+                                     max_col=2):
+              for cell in row:
+                  if (str(cell.value)) == "None":
+                      continue
+                  else:
+                      await message.channel.send(cell.value)
+    else:
+      await message.channel.send("Jutro nie ma zajęć. gramy gramy GRAMY")
   await client.process_commands(message)      
+  
 
 @client.event
 async def on_command_error(ctx, error):
 	if isinstance(error, commands.CommandNotFound):
-		await ctx.send('Nie ma takiej komendy')
+		await ctx.send('Nie ma takowej komendy')
 
-@client.command()
+@client.command(help='Sprawdza ping')
 async def ping(ctx):
-	await ctx.channel.send("pong")
+	await ctx.channel.send(f'pong odbity z prędkością {round(client.latency * 1000)} ms')
 
-# @client.command()
-# async def przerwa(ctx):
-# 	await ctx.channel.send("Przewidywana przerwa techniczna w godzinach 17-23")
 
+@client.command(help='polo')
+async def marco(ctx):
+	await ctx.channel.send("polo")
+
+
+@client.command(help='Wypisuje zmiany po najnowszej aktualizacji.')
+async def aktualizacja(ctx):
+  await ctx.channel.send("Od ostatniej aktualziacji dodano kolejkowanie bota muzycznego, od teraz można zakolejkować kilka piosenek, które odtworzą się jedna po drugiej! Oprócz tego poprawiono wiele pomniejszych błędów. Miłej zabawy ")
+
+
+@client.command(help='Wysyła mema')
+async def memy(ctx):
+  szukam = await ctx.channel.send("szukam...")
+  for i in range(100):
+    url = 'https://meme-api.herokuapp.com/gimme/ProgrammerHumor'
+    resp = requests.get(url=url)
+    data = resp.json()
+    print(data["ups"])
+    if data["ups"] > 500:
+      break
+      # print(data)
+      # print(data["url"])
+
+  embed=discord.Embed(title="Memik", url="", description="quality: " + str(data["ups"]), color=0x42cbf5)
+  embed.set_image(url=data["url"])
+  #msg = data["url"] + " tekst"
+  await szukam.delete()
+  await ctx.channel.send(embed=embed)
+  #await ctx.delete()
 
 print(db["encouragements"])
 keep_alive()
--- music.py
+++ music.py
@@ -1,111 +1,150 @@
 import discord
 import os
+from random import choice
 import urllib.parse, urllib.request, re
 from discord.ext import commands
 import youtube_dl
 from discord.utils import get
+import threading
+import asyncio
+
+client = commands.Bot(command_prefix="!", intents = discord.Intents.all())
+
+global queue
+queue=[]
+loop = False
+
+
 
 
 class music(commands.Cog):
   def __init__(self,client):
     self.client = client
 
+
+
+  
+  global is_connected
   def is_connected(ctx):
     voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
     return voice_client and voice_client.is_connected()
-  # def is_connected(ctx):
-  #   voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
-  #   return voice_client and voice_client.is_connected()
-  
-  @commands.command()
+    
+
+  global pobieranie0
+  def pobieranie0 (url):
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(pobieranie(url))
+    loop.close()
+
+  global check_queue
+  def check_queue(vc):
+    print('witam')
+    if len(queue)>0:
+      pobieranie0(queue[0])
+      vc.play(source, after=lambda x=0: check_queue(vc))
+      print("gram")
+      if not loop:
+        del queue[0]
+      
+  global pobieranie
+  async def pobieranie(url):
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 1000000000000M', 'options': '-vn -probesize 1000000000000M'}
+    YDL_OPTIONS = {'default_search': 'auto','format':"bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]"}
+    with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+      info = ydl.extract_info(queue[0], download=False)
+      url2 = info['formats'][0]['url']
+      global source
+      source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+
+  @commands.command(help='Bot dołącza do kanału głosowego.')
   async def join(self,ctx):
     voice_channel = ctx.author.voice.channel
     if ctx.author.voice is None:
-      await ctx.send("Nie jestes")
+      await ctx.send("Nie jestes na kanale")
     if ctx.voice_client is None:
       await voice_channel.connect()
     else:
         await ctx.voice_client.move_to(voice_channel)
+  
+  @commands.command(help='Rozłącza bota z kanałem.')
+  async def leave(self,ctx):
+    await ctx.voice_client.disconnect()
 
-  @commands.command()
-  async def play(self,ctx,url):
+  @commands.command(name='view', help='Wyświetla piosenki w kolejce.')
+  async def view(self, ctx):
+    await ctx.channel.send(f'Obecna kolejka: `{queue}!`')
+ 
+  @commands.command(name='play', help='Odtwarza muzykę. Po spacji należy podać link do yt z piosenką albo mniej więcej nazwę piosenki.')
+  async def play(self,ctx,*,url):
     global info
-    def is_connected(ctx):
-      voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
-      return voice_client and voice_client.is_connected()
+    global url2
+    global vc
+    global link
+    global htpfinder
+    global source
     voice_channel = ctx.author.voice.channel
-    if is_connected(ctx):
+    if not is_connected(ctx):
+      await voice_channel.connect()
+    query_string = urllib.parse.urlencode({'search_query': url})
+    html_content = urllib.request.urlopen('http://www.youtube.com/results?' + query_string)
+    search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
+    finder = search_results[0]
+    htpfinder = 'https://www.youtube.com/watch?v=' + finder
+    FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 1000000000000M', 'options': '-vn -probesize 1000000000000M'}
+    YDL_OPTIONS = {'default_search': 'auto','format':"bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]"}
+    vc = ctx.voice_client
+    if not url.startswith('https://www.youtube'):
+      url=htpfinder
+      await ctx.send("Właśnie odpalam: " + url)
+    #await threading.Thread(target = petle0, args=(ctx, url)).start()
+    if not ctx.voice_client.is_playing():
+      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+        htpfinder = url
+        vc.play(source, after=lambda x=0: check_queue(vc))
+        #queue.append(url)
+    elif ctx.voice_client.is_playing():
+      queue.append(url)
+
       #query_string = urllib.parse.urlencode({'search_query': search})
       #html_content = urllib.request.urlopen('http://www.youtube.com/results?' + query_string)
       #search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
       #await ctx.send('!play' + 'http://www.youtube.com/watch?v=' + search_results[0])
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 1000000000000M', 'options': '-vn -probesize 1000000000000M'}
-      YDL_OPTIONS = {'default_search': 'auto','format':"bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]"}
-      global vc
-      vc = ctx.voice_client
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        global link
-        global mus
-        link = url
-        mus=source
-        vc.play(mus)
-        
-    else:
-      await voice_channel.connect()
-      #ctx.voice_client.stop()
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5  -probesize 1000000000000M', 'options': '-vn -probesize 1000000000000M'}
-      YDL_OPTIONS = {'format':"bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]"}
-      vc = ctx.voice_client
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(url, download=False)
-        url2 = info['formats'][0]['url']
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        link = url
-        mus=source
-        vc.play(mus)
-
-  @commands.command()
-  async def loop(self, ctx):
-    if not ctx.voice_client.is_playing():
-      await ctx.reply("Nie ma czego zloopować")
-    ctx.voice_client.loop = not ctx.voice_client.loop
-    await ctx.message.add_reaction("👍")
   
-  @commands.command()
-  async def loop(self, ctx):
-    global loop
-    if loop:
-        await ctx.send('Loop mode is now `False!`')
-        loop = False
-    else: 
-        await ctx.send('Loop mode is now `True!`')
-        loop = True
-    
-  @commands.command()
+  @commands.command(help='Zatrzymuje graną przez bota piosenkę, można ją odpauzować komendą resume.')
   async def pause(self,ctx):
+      if queue:
+        print("list is not empty")
+      else:
+        print("list is empty")
       await ctx.voice_client.pause()
       await ctx.send("Paused")
 
-  @commands.command()
+  @commands.command(help='Wznawia zatrzymaną wcześniej piosenkę.')
   async def resume(self,ctx):
-      await ctx.voice_client.resume()
-      await ctx.send("Resume")
+    await ctx.voice_client.resume()
+    await ctx.send("Resume")
 
-  @commands.command()
-  async def leave(self,ctx):
-    await ctx.voice_client.disconnect()
+  @commands.command(help='Przerywa obecnie graną piosenkę i odtwarza kolejną z kolejki.')
+  async def skip(self,ctx):
+    await ctx.voice_client.stop()
+    await ctx.send("Stopped")  
+
+  @commands.command(help='Czyści kolejkę z wszystkich piosenek')
+  async def clear(self,ctx):
+    queue.clear()
     
-  @commands.command()
+  @commands.command(help='Pozwala sprawdzić, czy i jaka piosenka jest obecnie odtwarzana.')
   async def playingaudio(self,ctx):
     if ctx.voice_client.is_playing():
-      await ctx.reply("Playing: " + str(link))
+      await ctx.reply("Playing: " + str(htpfinder))
     else:
       await ctx.reply("Nothing is playing")
 
-  @commands.command()
+  @commands.command(help='Wyszukuje link do filmu na yt, którego nazwę należy podać po spacji.')
   async def find(self,ctx,*,search):
     global query_string
     global html_content
@@ -114,53 +153,19 @@ class music(commands.Cog):
     html_content = urllib.request.urlopen('http://www.youtube.com/results?' + query_string)
     search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
     await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
-    
-  @commands.command()
-  async def loop(self,ctx):
-    ctx.voice_client = music.get_player(guild_id=ctx.guild.id)
-    song = await ctx.voice_client.toggle_song_loop()
-    if song.is_looping:
-      await ctx.reply("loopujemy")
-    else:
-      await ctx.reply("nic nie loopuje")
 
-  @commands.command()
-  async def search(self,ctx,*,search):
-    query_string = urllib.parse.urlencode({'search_query': search})
-    html_content = urllib.request.urlopen('http://www.youtube.com/results?' + query_string)
-    search_results = re.findall(r"watch\?v=(\S{11})", html_content.read().decode())
-    finder = search_results[0]
-    htpfinder = 'http://www.youtube.com/watch?v=' + finder
-    #await ctx.send('http://www.youtube.com/watch?v=' + search_results[0])
-    def is_connected(ctx):
-      voice_client = get(ctx.bot.voice_clients, guild=ctx.guild)
-      return voice_client and voice_client.is_connected()
-    voice_channel = ctx.author.voice.channel
-    if is_connected(ctx):
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5 -probesize 1000000000000M', 'options': '-vn -probesize 1000000000000M'}
-      YDL_OPTIONS = {'default_search': 'auto','format':"bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]"}
-      vc = ctx.voice_client
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info = ydl.extract_info(htpfinder, download=False)
-        print(htpfinder)
-        url2 = info['formats'][0]['url']
-        #print(finder)
-        #print('siema' + url2)
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        vc.play(source)
-        
+  @commands.command(name = 'loop', help='Zapętla kolejną piosenkę')
+  async def loop_(self, ctx):
+    global loop
+    if loop:
+      await ctx.send("Wyłączono zapętlanie")
+      loop = False
+      if len(queue)>0:
+        del queue[0]
     else:
-      await voice_channel.connect()
-      #ctx.voice_client.stop()
-      FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5  -probesize 1000000000000M', 'options': '-vn -probesize 1000000000000M'}
-      YDL_OPTIONS = {'format':"bestvideo[ext=mp4]+bestaudio[ext=mp4]/mp4+best[height<=480]"}
-      vc = ctx.voice_client
-      with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
-        info2 = ydl.extract_info(htpfinder, download=False)
-        url2 = info2['formats'][0]['url']
-        print(htpfinder)
-        source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        vc.play(source)
+      await ctx.send("Następna piosenka zsotanie zapętlona!")
+      loop = True
+
     
 def setup(client):
     client.add_cog(music(client))
